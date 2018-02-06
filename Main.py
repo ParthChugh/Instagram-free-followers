70584ae7bc029e5973ae28167121684abc2ebcdd
parthchugh <parthchugh52@gmail.com>
2018-02-06 10:46:50 +0530

    After every Iteration it will add the login and password into the excel file

--- Main.py
+++ Main.py
@@ -2,31 +2,51 @@ import pyautogui, sys, os
 import webbrowser
 import time
 import names,string,random
+from openpyxl import load_workbook
 
     
 def random_char(y):
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
 
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito'
-for i in range(1,50):
+for i in range(1,2):
+    from openpyxl import load_workbook
+    wb = load_workbook('Names.xlsx')
+    ws = wb.active
+
     os.system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.instagram.com/?hl=en' )")
+    time.sleep(2)
+    pyautogui.click(x=372, y=241)
     pyautogui.hotkey('win','up')
-    time.sleep(12)
-    pyautogui.moveRel(0, 50)
+    time.sleep(14)
+    pyautogui.moveRel(0, 100)
     pyautogui.click(x=838, y=401)
+
     name = names.get_first_name()
-    pyautogui.typewrite(random_char(5)+'@abc.com')
+    last = names.get_last_name()
+
+    pyautogui.typewrite(random_char(5)+'@'+str(random_char(5))+ '.com')
     pyautogui.click(x=838, y=457)
     pyautogui.typewrite(name)
+    
     pyautogui.moveRel(0, 50)
     pyautogui.click(x=838, y=491)
-    pyautogui.typewrite(name+'BOTssssssss')
+
+    username = name+'_'+last+'_'+str(random.randint(400,1956))
+    pyautogui.typewrite(username)
     pyautogui.click(x=838, y=537)
-    pyautogui.typewrite('osheenBOT')
+    
+    password = 'OsheenBot'
+    pyautogui.typewrite(password)
+
+    ws.append([username,password])
+    wb.save("Names.xlsx")
+
     pyautogui.click(x=838, y=587)
-    time.sleep(3)
-    os.system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.instagram.com/technical_guruji999/?hl=en' )")
-    time.sleep(3)
-    pyautogui.click(x=860, y=215)
-    time.sleep(3)
+    time.sleep(4)
+    os.system("powershell -C Start-Process chrome.exe -ArgumentList @( '-incognito', 'https://www.instagram.com/travelandfood0055/?hl=en' )")
+    time.sleep(4)
+    pyautogui.click(x=840, y=215)
+    time.sleep(4)
     os.system("taskkill /im chrome.exe /f")
+    
